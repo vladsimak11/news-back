07b7423042b9481aadb6a1329cd7d1e78af72f40
Vladyslav Simak <simakv21@gmail.com>
2024-03-13 21:49:50 +0200

    get email

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,30 +2,41 @@ const { User } = require("../models/user");
 
 const { HttpError } = require("../helpers");
 
-const subscribe = async(req, res, next) => {
-  
+const subscribe = async (req, res, next) => {
   try {
     const { email } = req.body;
 
-    const user = await User.findOne({email});
-    
-    if(user) {
-      throw HttpError(409, "Email in use");
-    };
+    const user = await User.findOne({ email });
 
-    const newUser = await User.create({...req.body });
+    if (user) {
+      throw HttpError(409, "Email in use");
+    }
+
+    const newUser = await User.create({ ...req.body });
 
     res.status(201).json({
       user: {
         email: newUser.email,
-      }
+      },
     });
   } catch (error) {
     next(error);
   }
+};
 
+const getEmails = async (req, res, next) => {
+  try {
+    const emails = await User.find(req);
+    console.log(emails);
+    res.json({
+      emails,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
   subscribe,
-}
+  getEmails,
+};
--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-const { subscribe } = require("../../controllers/auth");
+const { subscribe, getEmails } = require("../../controllers/auth");
 
 router.post("/subscribe", subscribe);
+router.get("/subscribe", getEmails);
 
 module.exports = router;
